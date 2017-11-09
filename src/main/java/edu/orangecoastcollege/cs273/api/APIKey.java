8bb2e870f65e638f74348283bf9bc7efea1bc629
vincenthoang <vietnhoang7@gmail.com>
2017-11-09 15:46:22 -0800

    Converted List return values to Hashmap
    Signed-off-by: vincenthoang <vietnhoang7@gmail.com>

---
+++ src/main/java/edu/orangecoastcollege/cs273/api/APIKey.java
@@ -0,0 +1,11 @@
+package edu.orangecoastcollege.cs273.api;
+
+public class APIKey {
+    public static String getAPIKey() {
+        return "C0685DCF9F95DA1DA3572383A42DAD8D";
+    }
+
+    public static String getAPIDomain() {
+        return "systemdota";
+    }
+}
